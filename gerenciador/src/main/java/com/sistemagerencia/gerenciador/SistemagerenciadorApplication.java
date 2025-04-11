1cbd38a8c3ca89fac89e89835de7eb3565191669
Isaac Aires <playerredbrasil@gmail.com>
2025-04-10 21:34:11 -0300

    setup backend

---
+++ gerenciador/src/main/java/com/sistemagerencia/gerenciador/SistemagerenciadorApplication.java
@@ -0,0 +1,13 @@
+package com.sistemagerencia.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemagerenciadorApplication.class, args);
+	}
+
+}
